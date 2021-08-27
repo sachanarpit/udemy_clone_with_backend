ed0ac04faf369f1f1e20537d1f0747bd068e4934
rajujai <rajujaijangra@gmail.com>
2021-08-27 11:47:12 +0530

    bug fix 2

--- src/controllers/crud.controller.js
+++ src/controllers/crud.controller.js
@@ -1,24 +1,47 @@
-const post = (model)=> async (req,res) =>{
-    try{
-        const item = await model.create(req.body);
+const post = (model) => async (req, res) => {
+    try {
+        const item = await model.create(req.body)
         return res.status(201).send(item);
+    } catch (err) {
+        return res.status(400).send(err.message)
     }
-    catch (err){
-        return res.status(400).send(err.message);
+};
+const get = (model) => async (req, res) => {
+    try {
+        const items = await model.find().lean().exec();
+        return res.status(200).send(items);
+    } catch (err) {
+        return res.status(400).send(err.message)
     }
-}
-
-const get = (model)=> async (req,res) =>{
-    try{
-        const item = await model.find().lean().exec()
+};
+const getOne = (model) => async (req, res) => {
+    try {
+        const item = await model.findById(req.params.id).lean().exec();
         return res.status(200).send(item);
-    }
-    catch (err){
+    } catch (err) {
         return res.status(400).send(err.message);
     }
-}
-
-module.exports = (model) =>({
+};
+const updateOne = (model) => async (req, res) => {
+    try {
+        const item = await model.findByIdAndUpdate(req.params.id, req.body, { new: true }).lean()
+        return res.status(205).send(item);
+    } catch (err) {
+        return res.status(400).send(err.message);
+    }
+};
+const deleteOne = (model, itemName) => async (req, res) => {
+    try {
+        const item = await model.findByIdAndDelete(req.params.id).lean()
+        return res.status(205).json({ [itemName]: item });
+    } catch (err) {
+        return res.status(400).send(err.message);
+    }
+};
+module.exports = (model, itemName) => ({
     post: post(model),
     get: get(model),
+    getOne: getOne(model),
+    updateOne: updateOne(model),
+    deleteOne: deleteOne(model, itemName)
 })
